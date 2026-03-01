f432f61d2401e46c331ee0005aab15edbb768e24
Alex-coder50 <thehonoredone995@gmail.com>
2026-03-01 19:35:45 +0200

    Add files via upload

---
+++ h5.py/h5.py
@@ -0,0 +1,5 @@
+alphabet = input("write somthin so i can check if its number of alphabet!!!")
+if alphabet.isalpha():
+    print("The input is an alphabet.")
+else:
+    print("The input is not an alphabet.")
