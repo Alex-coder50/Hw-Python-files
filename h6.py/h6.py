0dfb531b76c27d9073105d4f7ec16fcba5168aad
Alex-coder50 <thehonoredone995@gmail.com>
2026-03-07 17:35:17 +0200

    Add files via upload

---
+++ h6.py/h6.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age for this website!!!"))
+
+if age < 10:
+    print("you're too young, boi")
+else:
+    if age > 20:
+        print("sinä olet vanha")
+    else:
+        print("Perfect, you can enter")
